979ac496207b68a2debf0bd234af7016137a188e
HEltim7 <heltimc@gmail.com>
2023-08-06 22:40:46 +0800

    修正数组越界

--- Templates/ds/sparse_table.cpp
+++ Templates/ds/sparse_table.cpp
@@ -1,17 +1,21 @@
 template<int size,typename T=int> struct SparseTable {
-    constexpr static int M=__lg(size);
-    T arr[size],st[M][size];
+    constexpr static int M=__lg(size)+1;
+    T st[M][size];
+    
+    T merge(const T &x,const T &y) {
+        return max(x,y);
+    }
 
     void build(int n) {
-        for(int i=1;i<=n;i++) st[0][i]=arr[i];
+        // for(int i=1;i<=n;i++) st[0][i]=arr[i]; // todo
         for(int k=1,t=1<<k;k<M;k++,t<<=1)
             for(int i=1,j=i+t-1,mid=i+t/2;j<=n;i++,j++,mid++)
-                st[k][i]=max(st[k-1][i],st[k-1][mid]);
+                st[k][i]=merge(st[k-1][i],st[k-1][mid]);
     }
 
     T query(int l,int r) {
         if(r<l) return 0;
         int k=__lg(r-l+1);
-        return max(st[k][l],st[k][r-(1<<k)+1]);
+        return merge(st[k][l],st[k][r-(1<<k)+1]);
     }
 };
